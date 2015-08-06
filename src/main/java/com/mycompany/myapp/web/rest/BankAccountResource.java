ae6e8f55d56b796f74ada94acbc518fd025f658b
Julien Dubois <julien.dubois@gmail.com>
2015-08-06 15:03:00 +0200

    automatic project update

---
+++ src/main/java/com/mycompany/myapp/web/rest/BankAccountResource.java
@@ -0,0 +1,124 @@
+package com.mycompany.myapp.web.rest;
+
+import com.codahale.metrics.annotation.Timed;
+import com.mycompany.myapp.domain.BankAccount;
+import com.mycompany.myapp.repository.BankAccountRepository;
+import com.mycompany.myapp.web.rest.util.HeaderUtil;
+import com.mycompany.myapp.web.rest.dto.BankAccountDTO;
+import com.mycompany.myapp.web.rest.mapper.BankAccountMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+import javax.inject.Inject;
+import javax.validation.Valid;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+/**
+ * REST controller for managing BankAccount.
+ */
+@RestController
+@RequestMapping("/api")
+public class BankAccountResource {
+
+    private final Logger log = LoggerFactory.getLogger(BankAccountResource.class);
+
+    @Inject
+    private BankAccountRepository bankAccountRepository;
+
+    @Inject
+    private BankAccountMapper bankAccountMapper;
+
+    /**
+     * POST  /bankAccounts -> Create a new bankAccount.
+     */
+    @RequestMapping(value = "/bankAccounts",
+            method = RequestMethod.POST,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    @Timed
+    public ResponseEntity<BankAccountDTO> create(@Valid @RequestBody BankAccountDTO bankAccountDTO) throws URISyntaxException {
+        log.debug("REST request to save BankAccount : {}", bankAccountDTO);
+        if (bankAccountDTO.getId() != null) {
+            return ResponseEntity.badRequest().header("Failure", "A new bankAccount cannot already have an ID").body(null);
+        }
+        BankAccount bankAccount = bankAccountMapper.bankAccountDTOToBankAccount(bankAccountDTO);
+        BankAccount result = bankAccountRepository.save(bankAccount);
+        return ResponseEntity.created(new URI("/api/bankAccounts/" + result.getId()))
+                .headers(HeaderUtil.createEntityCreationAlert("bankAccount", result.getId().toString()))
+                .body(bankAccountMapper.bankAccountToBankAccountDTO(result));
+    }
+
+    /**
+     * PUT  /bankAccounts -> Updates an existing bankAccount.
+     */
+    @RequestMapping(value = "/bankAccounts",
+        method = RequestMethod.PUT,
+        produces = MediaType.APPLICATION_JSON_VALUE)
+    @Timed
+    public ResponseEntity<BankAccountDTO> update(@Valid @RequestBody BankAccountDTO bankAccountDTO) throws URISyntaxException {
+        log.debug("REST request to update BankAccount : {}", bankAccountDTO);
+        if (bankAccountDTO.getId() == null) {
+            return create(bankAccountDTO);
+        }
+        BankAccount bankAccount = bankAccountMapper.bankAccountDTOToBankAccount(bankAccountDTO);
+        BankAccount result = bankAccountRepository.save(bankAccount);
+        return ResponseEntity.ok()
+                .headers(HeaderUtil.createEntityUpdateAlert("bankAccount", bankAccountDTO.getId().toString()))
+                .body(bankAccountMapper.bankAccountToBankAccountDTO(result));
+    }
+
+    /**
+     * GET  /bankAccounts -> get all the bankAccounts.
+     */
+    @RequestMapping(value = "/bankAccounts",
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    @Timed
+    @Transactional(readOnly = true)
+    public List<BankAccountDTO> getAll() {
+        log.debug("REST request to get all BankAccounts");
+        return bankAccountRepository.findAll().stream()
+            .map(bankAccount -> bankAccountMapper.bankAccountToBankAccountDTO(bankAccount))
+            .collect(Collectors.toCollection(LinkedList::new));
+    }
+
+    /**
+     * GET  /bankAccounts/:id -> get the "id" bankAccount.
+     */
+    @RequestMapping(value = "/bankAccounts/{id}",
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    @Timed
+    public ResponseEntity<BankAccountDTO> get(@PathVariable Long id) {
+        log.debug("REST request to get BankAccount : {}", id);
+        return Optional.ofNullable(bankAccountRepository.findOne(id))
+            .map(bankAccountMapper::bankAccountToBankAccountDTO)
+            .map(bankAccountDTO -> new ResponseEntity<>(
+                bankAccountDTO,
+                HttpStatus.OK))
+            .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    /**
+     * DELETE  /bankAccounts/:id -> delete the "id" bankAccount.
+     */
+    @RequestMapping(value = "/bankAccounts/{id}",
+            method = RequestMethod.DELETE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    @Timed
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        log.debug("REST request to delete BankAccount : {}", id);
+        bankAccountRepository.delete(id);
+        return ResponseEntity.ok().headers(HeaderUtil.createEntityDeletionAlert("bankAccount", id.toString())).build();
+    }
+}
---
+++ src/main/java/com/mycompany/myapp/web/rest/dto/BankAccountDTO.java
@@ -0,0 +1,97 @@
+package com.mycompany.myapp.web.rest.dto;
+
+import javax.validation.constraints.*;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Objects;
+
+
+/**
+ * A DTO for the BankAccount entity.
+ */
+public class BankAccountDTO implements Serializable {
+
+    private Long id;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private BigDecimal balance;
+
+    private Long userId;
+
+    private String userLogin;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUserLogin() {
+        return userLogin;
+    }
+
+    public void setUserLogin(String userLogin) {
+        this.userLogin = userLogin;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        BankAccountDTO bankAccountDTO = (BankAccountDTO) o;
+
+        if ( ! Objects.equals(id, bankAccountDTO.id)) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "BankAccountDTO{" +
+                "id=" + id +
+                ", name='" + name + "'" +
+                ", balance='" + balance + "'" +
+                '}';
+    }
+}
