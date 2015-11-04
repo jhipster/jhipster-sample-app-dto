c266a8cb278770b279fc7483dacbc63c43bbbb4e
Julien Dubois <julien.dubois@gmail.com>
2015-11-04 17:37:33 +0100

    automatic project update

--- src/main/webapp/scripts/app/entities/label/label.controller.js
+++ src/main/webapp/scripts/app/entities/label/label.controller.js
@@ -2,6 +2,7 @@
 
 angular.module('sampleDTOApp')
     .controller('LabelController', function ($scope, $state, $modal, Label) {
+      
         $scope.labels = [];
         $scope.loadAll = function() {
             Label.query(function(result) {
