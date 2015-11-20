4a17cd0f5243bfa8c1c86d1b2f048c00e0304d79
Julien Dubois <julien.dubois@gmail.com>
2015-11-20 16:07:50 +0100

    automatic project update

--- src/main/webapp/scripts/app/admin/metrics/metrics.controller.js
+++ src/main/webapp/scripts/app/admin/metrics/metrics.controller.js
@@ -49,7 +49,7 @@ angular.module('sampleDTOApp')
                     size: 'lg',
                     resolve: {
                         threadDump: function() {
-                            return data;
+                            return data.content;
                         }
 
                     }
