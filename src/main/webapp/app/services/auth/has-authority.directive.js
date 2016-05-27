2d2a0d1db6d58af04358e54c652723373a4a02b5
Julien Dubois <julien.dubois@gmail.com>
2016-05-27 11:52:37 +0200

    automatic project update

--- src/main/webapp/app/services/auth/has-authority.directive.js
+++ src/main/webapp/app/services/auth/has-authority.directive.js
@@ -5,6 +5,8 @@
         .module('jhipsterDtoSampleApplicationApp')
         .directive('hasAuthority', hasAuthority);
 
+    hasAuthority.$inject = ['Principal'];
+
     function hasAuthority(Principal) {
         var directive = {
             restrict: 'A',
