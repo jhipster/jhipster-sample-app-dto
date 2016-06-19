50c7c012a92a905d928737b236c8c2821d786a98
Julien Dubois <julien.dubois@gmail.com>
2016-06-19 23:41:46 +0200

    automatic project update

--- src/main/java/io/github/jhipster/sample/web/rest/OperationResource.java
+++ src/main/java/io/github/jhipster/sample/web/rest/OperationResource.java
@@ -15,7 +15,6 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import javax.inject.Inject;
