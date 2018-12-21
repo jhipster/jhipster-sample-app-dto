1502960cc76fb4cb02a2af9555053dbad601b881
Julien Dubois <julien.dubois@gmail.com>
2018-12-21 12:06:13 +0100

    Initial application generated by JHipster-5.7.2

--- src/main/webapp/app/shared/shared-common.module.ts
+++ src/main/webapp/app/shared/shared-common.module.ts
@@ -3,8 +3,8 @@ import { NgModule } from '@angular/core';
 import { JhipsterDtoSampleApplicationSharedLibsModule, FindLanguageFromKeyPipe, JhiAlertComponent, JhiAlertErrorComponent } from './';
 
 @NgModule({
-  imports: [JhipsterDtoSampleApplicationSharedLibsModule],
-  declarations: [FindLanguageFromKeyPipe, JhiAlertComponent, JhiAlertErrorComponent],
-  exports: [JhipsterDtoSampleApplicationSharedLibsModule, FindLanguageFromKeyPipe, JhiAlertComponent, JhiAlertErrorComponent]
+    imports: [JhipsterDtoSampleApplicationSharedLibsModule],
+    declarations: [FindLanguageFromKeyPipe, JhiAlertComponent, JhiAlertErrorComponent],
+    exports: [JhipsterDtoSampleApplicationSharedLibsModule, FindLanguageFromKeyPipe, JhiAlertComponent, JhiAlertErrorComponent]
 })
 export class JhipsterDtoSampleApplicationSharedCommonModule {}
--- src/main/webapp/app/shared/shared.module.ts
+++ src/main/webapp/app/shared/shared.module.ts
@@ -3,24 +3,24 @@ import { NgbDateAdapter } from '@ng-bootstrap/ng-bootstrap';
 
 import { NgbDateMomentAdapter } from './util/datepicker-adapter';
 import {
-  JhipsterDtoSampleApplicationSharedLibsModule,
-  JhipsterDtoSampleApplicationSharedCommonModule,
-  JhiLoginModalComponent,
-  HasAnyAuthorityDirective
+    JhipsterDtoSampleApplicationSharedLibsModule,
+    JhipsterDtoSampleApplicationSharedCommonModule,
+    JhiLoginModalComponent,
+    HasAnyAuthorityDirective
 } from './';
 
 @NgModule({
-  imports: [JhipsterDtoSampleApplicationSharedLibsModule, JhipsterDtoSampleApplicationSharedCommonModule],
-  declarations: [JhiLoginModalComponent, HasAnyAuthorityDirective],
-  providers: [{ provide: NgbDateAdapter, useClass: NgbDateMomentAdapter }],
-  entryComponents: [JhiLoginModalComponent],
-  exports: [JhipsterDtoSampleApplicationSharedCommonModule, JhiLoginModalComponent, HasAnyAuthorityDirective],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+    imports: [JhipsterDtoSampleApplicationSharedLibsModule, JhipsterDtoSampleApplicationSharedCommonModule],
+    declarations: [JhiLoginModalComponent, HasAnyAuthorityDirective],
+    providers: [{ provide: NgbDateAdapter, useClass: NgbDateMomentAdapter }],
+    entryComponents: [JhiLoginModalComponent],
+    exports: [JhipsterDtoSampleApplicationSharedCommonModule, JhiLoginModalComponent, HasAnyAuthorityDirective],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class JhipsterDtoSampleApplicationSharedModule {
-  static forRoot() {
-    return {
-      ngModule: JhipsterDtoSampleApplicationSharedModule
-    };
-  }
+    static forRoot() {
+        return {
+            ngModule: JhipsterDtoSampleApplicationSharedModule
+        };
+    }
 }
