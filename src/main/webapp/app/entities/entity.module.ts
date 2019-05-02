3055ac788a5c797a73e63480284fafafd55ec861
Julien Dubois <julien.dubois@gmail.com>
2019-05-02 22:07:28 +0200

    Initial application generated by JHipster-6.0.0

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -2,26 +2,26 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @NgModule({
-    imports: [
-        RouterModule.forChild([
-            {
-                path: 'bank-account',
-                loadChildren: './bank-account/bank-account.module#JhipsterDtoSampleApplicationBankAccountModule'
-            },
-            {
-                path: 'label',
-                loadChildren: './label/label.module#JhipsterDtoSampleApplicationLabelModule'
-            },
-            {
-                path: 'operation',
-                loadChildren: './operation/operation.module#JhipsterDtoSampleApplicationOperationModule'
-            }
-            /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
-        ])
-    ],
-    declarations: [],
-    entryComponents: [],
-    providers: [],
-    schemas: [CUSTOM_ELEMENTS_SCHEMA]
+  imports: [
+    RouterModule.forChild([
+      {
+        path: 'bank-account',
+        loadChildren: './bank-account/bank-account.module#JhipsterDtoSampleApplicationBankAccountModule'
+      },
+      {
+        path: 'label',
+        loadChildren: './label/label.module#JhipsterDtoSampleApplicationLabelModule'
+      },
+      {
+        path: 'operation',
+        loadChildren: './operation/operation.module#JhipsterDtoSampleApplicationOperationModule'
+      }
+      /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
+    ])
+  ],
+  declarations: [],
+  entryComponents: [],
+  providers: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class JhipsterDtoSampleApplicationEntityModule {}
